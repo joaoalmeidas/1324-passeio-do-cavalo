f1792bc30e45584f430b33584d88f9230a7bce83
joao <joao182cx@gmail.com>
2019-02-05 09:24:03 -0200

    Criação da classe Cavalo2.

---
+++ src/Cavalo2.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Cavalo2 {
+	
+	private final static int[] horizontal = {2, 1, -1, -2, -2, -1, 1, 2};
+	private final static int[] vertical = {-1, -2, -2, -1, 1, 2, 2, 1};
+	
+}
--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.Random;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
