be50305113b9a46d7a94e044d601634ac353b0dd
joao <joao182cx@gmail.com>
2019-02-04 23:48:20 -0200

    Criação da classe CavaloFrame.

---
+++ src/CavaloFrame.java
@@ -0,0 +1,5 @@
+import javax.swing.JFrame;
+
+public class CavaloFrame extends JFrame {
+
+}
--- src/Tabuleiro.java
+++ src/Tabuleiro.java
@@ -1,4 +1,5 @@
+import javax.swing.JPanel;
 
-public class Tabuleiro {
+public class Tabuleiro extends JPanel {
 
 }
